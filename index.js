094187c0804e8cf922bbcccd06c3c0ad4c2872d5
Alys Ferragamo <alysferr@gmail.com>
2019-06-21 20:17:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function logShout(string){
   return string.toLowerCase()
 }
 var uppercase = "HELLO!"
+var lowercase = "hello!"
 
 function sayHiToGrandma(string){
   if ( string.toUpperCase ===uppercase){
