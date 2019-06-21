ae6d4514ea11fd054325fc7a53ab5b45e1d8b638
Alys Ferragamo <alysferr@gmail.com>
2019-06-21 20:17:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,5 +16,7 @@ function sayHiToGrandma(string){
   if (string.toLowerCase === lowercase){
     return "I can't hear you"
   }
-  if(string == "I love ")
+  if(string == "I love you, Grandma."){
+    return "I love you, too"
+  }
 }
