21824485ee9780f863b463a6993cd998d8b44cdb
Alys Ferragamo <alysferr@gmail.com>
2019-06-21 20:24:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function sayHiToGrandma(string){
   if (string === uppercase){
     return 'YES INDEED!'}
   if (string === lowercase){
-    return 'I can\'t hear you'
+    return 'I can\'t hear you!'
   }
   if(string === "I love you, Grandma."){
     return 'I love you, too.'
